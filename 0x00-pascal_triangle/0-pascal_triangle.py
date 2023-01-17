fa4060f2da5b8eea0e1675841f6a6dfc33f6da08
Gejix <geraldjuwah@gmail.com>
2023-01-17 23:49:10 +0000

    [feat]:Second commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""Pascal's Triangle"""
+
+"""
+Pascal's Triangle
+"""
+
 
 def pascal_triangle(n):
-	"""
-	a function that returns a list
-	of integers representing the
-	pascal triangle of n:
-		. Returns an empty list if n <= 0
-		. assume n will be always an integer
-	"""
-	pascal_tri = []
-
-	if n <= 0:
-		return []
-
-	for i in range(n):
-		if (i == 0):
-			pascal_tri.append([1])
-		else:
-			cur_row = []
-			for j in range(i + 1):
-				if (j == 0 or j == i):
-					cur_row.append(1)
-				else:
-					cur_row.append(pascal_tri[i - 1][j - 1] +
-									pascal_tri[i - 1][j])
-
-			pascal_tri.append(cur_row)
-
-	return (pascal_tri)
+    """Print Pascal's Triangle
+    Args:
+        n (int): Size of the pascal triangle
+    """
+    res = []
+    if (n <= 0):
+        return res
+    else:
+        for x in range(n+1):
+            temp = []
+            # first element is always 1
+            c = 1
+            for y in range(1, x+1):
+                # first value in a line is always 1
+                temp.append(c)
+                # using Binomial Coefficient
+                c = c * (x - y) // y
+            if (len(temp)):
+                res.append(temp)
+    return res
